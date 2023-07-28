f89d73657b1a426882632fd40909b74ccff5dcc1
Kieron Harrower <somebody.else.2017@gmail.com>
2023-07-28 13:57:28 +0100

    page for indiviual posts working & shows post's comments

--- controllers/posts_controller.py
+++ controllers/posts_controller.py
@@ -7,7 +7,12 @@ posts_blueprint=Blueprint("posts",__name__)
 
 @posts_blueprint.route("/")
 def feed():
-    only_posts=Post.query.all() # sort by time of last engagement (maybe just reverse the list, could edit a post's table entry & edit it right back to what it was each time a comment is made to 'bump' it down to the bottom of the posts table in the db)
-    comment_counts=[post.count_comments() for post in only_posts]
-    posts=[[only_posts[i],comment_counts[i]] for i in range(len(only_posts))]
+    posts_no_comments=Post.query.all() # sort by time of last engagement (maybe just reverse the list, could edit a post's table entry & edit it right back to what it was each time a comment is made to 'bump' it down to the bottom of the posts table in the db)
+    posts=[[post,post.count_comments()] for post in posts_no_comments]
     return render_template("index.jinja",posts=posts)
+
+@posts_blueprint.route("/<id>")
+def show_post(id):
+    post=Post.query.get(id)
+    comments=post.get_comments()
+    return render_template("show_post.jinja",post=post,comments=comments)
