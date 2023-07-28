313b9d404ab30067e03bbeb565294f8c2ec22e4a
Kieron Harrower <somebody.else.2017@gmail.com>
2023-07-28 12:15:29 +0100

    never realised I have to migrate & upgrade when I add a new table, will know for future

--- controllers/posts_controller.py
+++ controllers/posts_controller.py
@@ -8,7 +8,6 @@ posts_blueprint=Blueprint("posts",__name__)
 @posts_blueprint.route("/")
 def feed():
     posts=Post.query.all() # sort by time of last engagement (maybe just reverse the list, could edit a post's table entry & edit it right back to what it was each time a comment is made to 'bump' it down to the bottom of the posts table in the db)
-    # comments=Comment.query.all()
-    # print(comments)
-    # return render_template("index.jinja",posts=posts)
-    return "hi"
+    comments=Comment.query.all()
+    print(comments)
+    return render_template("index.jinja",posts=posts)
