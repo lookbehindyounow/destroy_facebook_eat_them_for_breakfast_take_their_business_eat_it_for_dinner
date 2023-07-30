e343a18e88628807620a897577a64defd6f6ead3
Kieron Harrower <somebody.else.2017@gmail.com>
2023-07-30 18:55:28 +0100

    users can now edit & delete their own posts & comments

--- controllers/posts_controller.py
+++ controllers/posts_controller.py
@@ -5,39 +5,80 @@ import datetime # add time to posts & comments
 
 posts_blueprint=Blueprint("posts",__name__)
 
-@posts_blueprint.route("/<user_id>")
-def feed(user_id):
+@posts_blueprint.route("/<int:user_id>")
+def show_feed(user_id):
     posts_no_comments=Post.query.all() # sort by time of last engagement (maybe just reverse the list, could edit a post's table entry & edit it right back to what it was each time a comment is made to 'bump' it down to the bottom of the posts table in the db)
     posts=[[post,post.return_name(),post.count_comments()] for post in posts_no_comments]
     return render_template("index.jinja",user_id=user_id,posts=posts)
 
-@posts_blueprint.route("/<user_id>/<post_id>")
+@posts_blueprint.route("/<int:user_id>/<int:post_id>")
 def show_post(user_id,post_id):
     post=Post.query.get(post_id)
     author=post.return_name()
     comments=post.get_comments()
     return render_template("show_post.jinja",user_id=user_id,post=post,author=author,comments=comments)
 
-@posts_blueprint.route("/<user_id>/new_post")
-def post_form(user_id):
-    return render_template("new.jinja",user_id=user_id,post=None)
+@posts_blueprint.route("/<int:user_id>/new_post_form")
+def new_post_form(user_id):
+    return render_template("new.jinja",user_id=user_id,role="Post")
 
-@posts_blueprint.route("/<user_id>",methods=["POST"])
-def post_post(user_id):
+@posts_blueprint.route("/<int:user_id>",methods=["POST"])
+def new_post(user_id):
     post=Post(user_id=user_id,content=request.form["content"])
     db.session.add(post)
     db.session.commit()
     return redirect(f"/{user_id}")
 
-@posts_blueprint.route("/<user_id>/<post_id>/new_comment")
-def comment_form(user_id,post_id):
+@posts_blueprint.route("/<int:user_id>/<int:post_id>/new_comment_form")
+def new_comment_form(user_id,post_id):
     post=Post.query.get(post_id)
     comments=post.get_comments()
-    return render_template("new.jinja",user_id=user_id,post=post,comments=comments)
+    return render_template("new.jinja",user_id=user_id,role="Comment",post=post,comments=comments)
 
-@posts_blueprint.route("/<user_id>/<post_id>",methods=["POST"])
-def post_comment(user_id,post_id):
+@posts_blueprint.route("/<int:user_id>/<int:post_id>",methods=["POST"])
+def new_comment(user_id,post_id):
     comment=Comment(user_id=user_id,post_id=post_id,content=request.form["content"])
     db.session.add(comment)
+    db.session.commit()
+    return redirect(f"/{user_id}/{post_id}")
+
+@posts_blueprint.route("/<int:user_id>/<int:post_id>/edit_post_form")
+def edit_post_form(user_id,post_id):
+    post=Post.query.get(post_id)
+    return render_template("new.jinja",user_id=user_id,role="Update Post",post=post)
+
+@posts_blueprint.route("/<int:user_id>/<int:post_id>/edit_post",methods=["POST"])
+def edit_post(user_id,post_id):
+    post=Post.query.get(post_id)
+    post.content=request.form["content"]
+    db.session.commit()
+    return redirect(f"/{user_id}/{post_id}")
+
+@posts_blueprint.route("/<int:user_id>/<int:post_id>/delete_post")
+def delete_post(user_id,post_id):
+    post=Post.query.get(post_id)
+    comments=post.get_comments()
+    [db.session.delete(comment[0]) for comment in comments]
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(f"/{user_id}")
+
+@posts_blueprint.route("/<int:user_id>/<int:post_id>/<int:comment_id>/edit_comment_form")
+def edit_comment_form(user_id,post_id,comment_id):
+    post=Post.query.get(post_id)
+    comment=Comment.query.get(comment_id)
+    return render_template("new.jinja",user_id=user_id,role="Update Comment",post=post,comment=comment)
+
+@posts_blueprint.route("/<int:user_id>/<int:post_id>/<int:comment_id>/edit_comment",methods=["POST"])
+def edit_comment(user_id,post_id,comment_id):
+    comment=Comment.query.get(comment_id)
+    comment.content=request.form["content"]
+    db.session.commit()
+    return redirect(f"/{user_id}/{post_id}")
+
+@posts_blueprint.route("/<int:user_id>/<int:post_id>/<int:comment_id>/delete_comment")
+def delete_comment(user_id,post_id,comment_id):
+    comment=Comment.query.get(comment_id)
+    db.session.delete(comment)
     db.session.commit()
     return redirect(f"/{user_id}/{post_id}")
