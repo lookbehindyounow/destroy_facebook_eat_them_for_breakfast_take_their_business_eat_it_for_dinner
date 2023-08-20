b8ab7c8618ec65fed21f33691ac32ba9f86fd6a9
Kieron Harrower <somebody.else.2017@gmail.com>
2023-08-20 18:34:41 +0100

    finished annotating & found a bug in approve()

--- controllers/posts_controller.py
+++ controllers/posts_controller.py
@@ -1,115 +1,116 @@
 from flask import render_template, redirect, Blueprint, request
 from models import User, Post, Comment, Approval, Friendship
 from app import db
-from datetime import datetime
+from datetime import datetime # for new_post() & new_comment()
 
 posts_blueprint=Blueprint("posts",__name__)
 
 @posts_blueprint.route("/<int:user_id>")
-def show_feed(user_id):
+def show_feed(user_id): # home feed page
+    # queries for posts that should show up
     friends_posts=Post.query.join(Friendship, Post.user_id==Friendship.friend_id).filter(user_id==Friendship.user_id).all()
     my_posts_and_public_posts=Post.query.filter((Post.user_id==user_id) | Post.public).all()
-    # talk about these queries & the duplicate removal below
-    posts=list(set(friends_posts+my_posts_and_public_posts))
-    [post.set_variables() for post in posts]
+    posts=list(set(friends_posts+my_posts_and_public_posts)) # add results & remove duplicates
+    [post.set_variables() for post in posts] # load any needed display information into post objects
+    # sort shown posts by the most recent time in the post's time & any of it's comments' times
     posts.sort(key=lambda post: max([post.time]+[comment.time for comment in post.comments]), reverse=True)
-    # talk about this^ sort
-    user=User.query.get(user_id) # only need for roulette
-    user.roulette.wheel=True
-    user.roulette.ball=True
+    user=User.query.get(user_id) # get user
+    user.roulette.wheel=True # set attributes in user's roulette object (unfinished feature but these
+    user.roulette.ball=True # booleans need to be true for the wheel & ball to spin)
+    # because isprofile=False, feed.jinja will extend index.jinja
     return render_template("feed.jinja",user_id=user_id,posts=posts,isprofile=False,roulette=user.roulette)
 
 @posts_blueprint.route("/<int:user_id>/<int:post_id>")
-def show_post(user_id,post_id):
+def show_post(user_id,post_id): # post page
     post=Post.query.get(post_id)
-    post.set_variables()
+    post.set_variables() # load display information
     return render_template("post.jinja",user_id=user_id,post=post)
 
 @posts_blueprint.route("/<int:user_id>/new_post_form")
-def new_post_form(user_id):
+def new_post_form(user_id): # loads new.jinja with role="Post"
     return render_template("composite/new.jinja",user_id=user_id,role="Post")
 
 @posts_blueprint.route("/<int:user_id>",methods=["POST"])
-def new_post(user_id):
+def new_post(user_id): # create new post
     post=Post(user_id=user_id,time=datetime.now(),public=("public" in request.form),content=request.form["content"])
     db.session.add(post)
     db.session.commit()
-    return redirect(f"/{user_id}")
+    return redirect(f"/{user_id}") # redirect to home feed
 
 @posts_blueprint.route("/<int:user_id>/<int:post_id>/new_comment_form")
-def new_comment_form(user_id,post_id):
-    post=Post.query.get(post_id)
+def new_comment_form(user_id,post_id): # loads new.jinja with role="Comment", extending post.jinja
+    post=Post.query.get(post_id) # so we need the post, because it will be displayed too
     post.set_variables()
     return render_template("composite/new.jinja",user_id=user_id,role="Comment",post=post)
 
 @posts_blueprint.route("/<int:user_id>/<int:post_id>",methods=["POST"])
-def new_comment(user_id,post_id):
+def new_comment(user_id,post_id): # create new comment
     comment=Comment(user_id=user_id,post_id=post_id,time=datetime.now(),content=request.form["content"])
     db.session.add(comment)
     db.session.commit()
-    return redirect(f"/{user_id}/{post_id}")
+    return redirect(f"/{user_id}/{post_id}") # redirect to parent post's page
 
 @posts_blueprint.route("/<int:user_id>/<int:post_id>/edit_post_form")
-def edit_post_form(user_id,post_id):
-    post=Post.query.get(post_id)
-    post.set_variables()
+def edit_post_form(user_id,post_id): # loads new.jinja with role="Update Post"
+    post=Post.query.get(post_id) # we need the post that's being edited, but we don't need the
+                                # display information from post.set_variables()
     return render_template("composite/new.jinja",user_id=user_id,role="Update Post",post=post)
 
 @posts_blueprint.route("/<int:user_id>/<int:post_id>/edit_post",methods=["POST"])
-def edit_post(user_id,post_id):
+def edit_post(user_id,post_id): # edit existing post
     post=Post.query.get(post_id)
     post.edit(request.form["content"],("public" in request.form))
-    return redirect(f"/{user_id}/{post_id}")
+    return redirect(f"/{user_id}/{post_id}") # redirect to post's page
+
+@posts_blueprint.route("/<int:user_id>/<int:post_id>/<int:comment_id>/edit_comment_form")
+def edit_comment_form(user_id,post_id,comment_id): # loads new.jinja with role="Update Comment"
+    post=Post.query.get(post_id) # post will be displayed
+    post.set_variables()
+    comment=Comment.query.get(comment_id) # comment object that's being edited
+    return render_template("composite/new.jinja",user_id=user_id,role="Update Comment",post=post,comment=comment)
+
+@posts_blueprint.route("/<int:user_id>/<int:post_id>/<int:comment_id>/edit_comment",methods=["POST"])
+def edit_comment(user_id,post_id,comment_id): # edit existing comment
+    comment=Comment.query.get(comment_id)
+    comment.edit(request.form["content"])
+    return redirect(f"/{user_id}/{post_id}") # redirect to parent post's page
 
 @posts_blueprint.route("/<int:user_id>/<int:post_id>/delete_post")
 def delete_post(user_id,post_id):
     post=Post.query.get(post_id)
-    post.set_variables()
-    [db.session.delete(approval) for approval in post.approvals]
+    post.set_variables() # get comments & approvals
+    [db.session.delete(approval) for approval in post.approvals] # cascading delete to keep database tidy
     [[db.session.delete(approval) for approval in comment.approvals] for comment in post.comments]
     [db.session.delete(comment) for comment in post.comments]
     db.session.delete(post)
     db.session.commit()
-    return redirect(f"/{user_id}")
-
-@posts_blueprint.route("/<int:user_id>/<int:post_id>/<int:comment_id>/edit_comment_form")
-def edit_comment_form(user_id,post_id,comment_id):
-    post=Post.query.get(post_id)
-    post.set_variables()
-    comment=Comment.query.get(comment_id)
-    return render_template("composite/new.jinja",user_id=user_id,role="Update Comment",post=post,comment=comment)
-
-@posts_blueprint.route("/<int:user_id>/<int:post_id>/<int:comment_id>/edit_comment",methods=["POST"])
-def edit_comment(user_id,post_id,comment_id):
-    comment=Comment.query.get(comment_id)
-    comment.edit(request.form["content"])
-    return redirect(f"/{user_id}/{post_id}")
+    return redirect(f"/{user_id}") # redirect to home feed
 
 @posts_blueprint.route("/<int:user_id>/<int:post_id>/<int:comment_id>/delete_comment")
 def delete_comment(user_id,post_id,comment_id):
     comment=Comment.query.get(comment_id)
-    comment.set_variables()
-    [db.session.delete(approval) for approval in comment.approvals]
+    comment.set_variables() # get approvals
+    [db.session.delete(approval) for approval in comment.approvals] # cascade
     db.session.delete(comment)
     db.session.commit()
-    return redirect(f"/{user_id}/{post_id}")
+    return redirect(f"/{user_id}/{post_id}") # redirect to parent post's page
 
 @posts_blueprint.route("/<int:user_id>/<int:post_id>/approve/<int:fromfeed>")
 @posts_blueprint.route("/<int:user_id>/<int:post_id>/<int:comment_id>/approve")
 def approve(user_id,post_id,comment_id=None,fromfeed=False):
-    if comment_id==None:
+    if comment_id==None: # if you've approved a post
         approvals=Approval.query.filter_by(user_id=user_id,post_id=post_id).all()
-    else:
+    else: # if you've approved a comment
         approvals=Approval.query.filter_by(user_id=user_id,comment_id=comment_id).all()
-    if len(approvals)==0:
-        if comment_id==None:
+    if len(approvals)==0: # if current user has no approvals for this post/comment
+        if comment_id==None: # create approval record for post/comment
             approval=Approval(user_id=user_id,ispost=True,post_id=post_id)
         else:
             approval=Approval(user_id=user_id,ispost=False,comment_id=comment_id)
         db.session.add(approval)
-    else:
-        [db.session.delete(approval) for approval in approvals] # talk about incase duplicate approval
+    else: # if current user has approvals for this post/comment (should only be 1):
+        [db.session.delete(approval) for approval in approvals] # remove them all (unapprove)
     db.session.commit()
-    if fromfeed: # talk about this condition
-        return redirect(f"/{user_id}")
+    if fromfeed: # if approving from feed, redirect to home feed, otherwise redirect to post page
+        return redirect(f"/{user_id}") # BUG just realised if approving from profile page feed, it redirects to home feed
     return redirect(f"/{user_id}/{post_id}")
--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -1,109 +1,124 @@
 from flask import render_template, redirect, Blueprint, request
 from models import User, Friend, Friendship, Post, Comment, Approval
 from app import db
-from controllers.posts_controller import delete_post, delete_comment
-from base64 import b64encode
+from controllers.posts_controller import delete_post, delete_comment # for delete_profile()
+from base64 import b64encode # for profile_pic()
 
 users_blueprint=Blueprint("users",__name__)
-login_message=False
+login_message=False # initialise login message control boolean because it needs to exist already
 
 @users_blueprint.route("/")
 @users_blueprint.route("/new_user")
-def login_page():
+def login_page(): # login/signup page depending on route
     global login_message
+    # if login_message has been set to true then it will be passed to login.jinja as temp_message
+    # which controls whether a message shows indicating a login/signup problem 
     temp_message=login_message
     login_message=False
-    if str(request.url_rule)=="/":
-        new=False
-        passwords=[user.password for user in User.query.all()]
-    else:
+    if str(request.url_rule)=="/": # if route is "/"
+        new=False # boolean controls login.jinja
+        passwords=[user.password for user in User.query.all()] # passwords for dropdown menu
+    else: # if route is "/new_user"
         new=True
         passwords=[]
     return render_template("login.jinja",new=new,login_message=temp_message,passwords=passwords)
 
 @users_blueprint.route("/login",methods=["POST"])
-def login():
+def login(): # existing users
+    # get first (should only be one) user  with matching username & password
     user=User.query.filter_by(name=request.form["name"],password=request.form["password"]).first()
-    if user==None:
+    if user==None: # if no matching user
         global login_message
         login_message=True
-        return redirect("/")
-    return redirect(f"/{user.id}")
+        return redirect("/") # redirect to login_page() with global login_message set to True
+    return redirect(f"/{user.id}") # otherwise redirect to user's home feed
 
 @users_blueprint.route("/signup",methods=["POST"])
-def signup():
+def signup(): # new users
+    # get all (should be one or none) users with matching username & password, if exists:
     if User.query.filter_by(name=request.form["name"],password=request.form["password"]).all():
         global login_message
         login_message=True
-        return redirect("/new_user")
-    user=User(name=request.form["name"],password=request.form["password"])
+        return redirect("/new_user") # redirect to login_page() with global login_message set to True
+    user=User(name=request.form["name"],password=request.form["password"]) # otherwise create user
     db.session.add(user)
     db.session.commit()
+    # matching record in friends table for friendships relationship table to work
     db.session.add(Friend(id=user.id))
     db.session.commit()
-    return redirect(f"/{user.id}")
+    return redirect(f"/{user.id}") # redirect to user's home feed
 
 @users_blueprint.route("/<int:user_id>/users")
-def show_users(user_id):
+def show_users(user_id): # list of all users page
     users=User.query.all()
-    friendships=Friendship.query.filter_by(user_id=user_id).all()
-    friends_ids=[friendship.friend_id for friendship in friendships]
+    # find ids of profiles with a friendship record with current user to identify in jinja who is a friend
+    friends_ids=Friendship.query.filter_by(user_id=user_id).with_entities(Friendship.friend_id).all()
+    friends_ids=[friend_id[0] for friend_id in friends_ids] # get ids from list of [(id, ),(id, ),(id, )...]
     return render_template("users/users.jinja",user_id=user_id,users=users,friends_ids=friends_ids)
 
 @users_blueprint.route("/<int:user_id>/users/<int:profile_id>/add")
 @users_blueprint.route("/<int:user_id>/profile/<int:profile_id>/add")
 def add_friend(user_id,profile_id):
-    db.session.add(Friendship(user_id=user_id,friend_id=profile_id))
-    db.session.add(Friendship(user_id=profile_id,friend_id=user_id))
+    db.session.add(Friendship(user_id=user_id,friend_id=profile_id)) # two records for each friendship, one
+    db.session.add(Friendship(user_id=profile_id,friend_id=user_id)) # each way round for simpler querying
     db.session.commit()
-    if "users" in str(request.url_rule): # talk about this condition
-        return redirect(f"/{user_id}/users")
+    if "users" in str(request.url_rule): # if route includes "users"; meaning we came from users list page
+        return redirect(f"/{user_id}/users") # redirect to users list
+    # otherwise redirect to whatever profile page we came from
     return redirect(f"/{user_id}/profile/{profile_id}")
 
 @users_blueprint.route("/<int:user_id>/users/<int:profile_id>/remove")
 @users_blueprint.route("/<int:user_id>/profile/<int:profile_id>/remove")
 def remove_friend(user_id,profile_id):
+    # find all friendship records between current user & profile in question, just incase of duplicates
+    # also have to find any matching records both ways round because there are two records for each friendship
     relations=Friendship.query.filter(((Friendship.user_id==user_id) & (Friendship.friend_id==profile_id)) |
                                     ((Friendship.user_id==profile_id) & (Friendship.friend_id==user_id))).all()
-    [db.session.delete(friendship) for friendship in relations] # talk about incase duplicate friendship
+    [db.session.delete(friendship) for friendship in relations] # delete all records found
     db.session.commit()
     if "users" in str(request.url_rule):
         return redirect(f"/{user_id}/users")
     return redirect(f"/{user_id}/profile/{profile_id}")
 
 @users_blueprint.route("/<int:user_id>/profile/<int:profile_id>")
-def show_profile(user_id,profile_id):
+def show_profile(user_id,profile_id): # profile page
     profile=User.query.get(profile_id)
+    # find profile's friends
     profile.friends_list=User.query.join(Friendship).filter(profile_id==Friendship.friend_id).all()
     if Friendship.query.filter_by(user_id=user_id,friend_id=profile_id).all() or user_id==profile_id:
+        # if selected profile is current user or a friend of current user, show all of profile's posts in feed
         posts=Post.query.filter_by(user_id=profile_id).all()
     else:
+        # if not, show only public posts from profile
         posts=Post.query.filter_by(user_id=profile_id,public=True).all()
-    [post.set_variables() for post in posts]
-    posts.sort(key=lambda post: post.time, reverse=True)
+    [post.set_variables() for post in posts] # load any needed display information into post objects
+    posts.sort(key=lambda post: post.time, reverse=True) # sort by time posted
+    # because isprofile=True, feed.jinja will extend profile.jinja
     return render_template("feed.jinja",user_id=user_id,profile=profile,posts=posts,isprofile=True)
 
 @users_blueprint.route("/<int:user_id>/profile/pfp",methods=["POST"])
-def profile_pic(user_id):
+def profile_pic(user_id): # upload pfp
     user=User.query.get(user_id)
+    # get file submitted to form in profile page (only available if it's current user's profile)
     binary_pfp=request.files["profile_picture"].read()
-    user.pfp=b64encode(binary_pfp).decode("utf-8")
+    user.pfp=b64encode(binary_pfp).decode("utf-8") # convert binary to base 64 & save to users table
     db.session.commit()
-    return redirect(f"/{user_id}/profile/{user_id}")
+    return redirect(f"/{user_id}/profile/{user_id}") # redirect to same page
 
 @users_blueprint.route("/<int:user_id>/profile/delete")
 def delete_profile(user_id):
-    posts=Post.query.filter_by(user_id=user_id).all()
-    [delete_post(user_id,post.id) for post in posts]
-    comments=Comment.query.filter_by(user_id=user_id).all()
-    [delete_comment(user_id,None,comment.id) for comment in comments]
-    approvals=Approval.query.filter_by(user_id=user_id).all()
+    posts=Post.query.filter_by(user_id=user_id).all() # get all of user's posts
+    [delete_post(user_id,post.id) for post in posts] # delete_post() cascades to child comments & approvals
+    comments=Comment.query.filter_by(user_id=user_id).all() # get all comments
+    [delete_comment(user_id,None,comment.id) for comment in comments] # also cascades
+    approvals=Approval.query.filter_by(user_id=user_id).all() # approvals
     [db.session.delete(approval) for approval in approvals]
+    # all friendships including user's id on either side
     friendships=Friendship.query.filter((Friendship.user_id==user_id) | (Friendship.friend_id==user_id)).all()
     [db.session.delete(friendship) for friendship in friendships]
-    friend=Friend.query.get(user_id)
+    friend=Friend.query.get(user_id) # user's record in the friends table
     db.session.delete(friend)
-    user=User.query.get(user_id)
+    user=User.query.get(user_id) # user's record in the user table
     db.session.delete(user)
     db.session.commit()
-    return redirect("/")
+    return redirect("/new_user") # redirect to signup page
